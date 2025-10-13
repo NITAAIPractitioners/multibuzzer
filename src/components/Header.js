4fbb6712675edc1b029b6965347f1de53e5c8462
NITAAIPractitioners <162414682+NITAAIPractitioners@users.noreply.github.com>
2025-10-13 22:34:41 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,7 @@ export default function Header({
     <header>
       <Navbar>
         <Navbar.Brand>
-          <Logo /> Multibuzzer
+          <Logo /> Sultan Almohammadi Crypto Course
         </Navbar.Brand>
         <div className="nav-buttons">
           {!isNil(sound) ? (
